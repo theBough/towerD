ca5c82e83345deaa58132815087cfd78a1066b32
corbin Boughen <50217007+theBough@users.noreply.github.com>
2026-02-25 11:45:49 -0600

    Initialize Player instances in setup function

--- sketch.js
+++ sketch.js
@@ -1,1 +1,14 @@
+let p, p2;
 
+
+function setup() {
+  createCanvas(400, 400);
+  p = new Player(200,200,20,20,"green")
+  p2 = new Player(200,250,20,20,"red")
+}
+
+function draw() {
+  background(220);
+  p.display();
+  p2.display();
+}
