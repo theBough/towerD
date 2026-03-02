9fa1dd0bcef934dbed31b1fca2959ad548abdef7
corbin Boughen <50217007+theBough@users.noreply.github.com>
2026-03-02 11:49:49 -0600

    Add enemy instance to sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 let p = [];
+let e;
 
 function setup() {
   createCanvas(400, 400);
   for (let i = 0; i < 5; i++) {
     p.push(new Player(200 + i * 20, 200, 20, 20, "green"));
   }
+  e = new Enemy(20, 100, 50,50,"images/cardinal1.png")
 }
 
 function draw() {
@@ -13,4 +15,5 @@ function draw() {
     p[i].display();
     p[i].update();
   }
+  e.display()
 }
