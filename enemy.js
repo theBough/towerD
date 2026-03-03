c02f69d36caea5325c86c6554f18450705e23b31
corbin Boughen <50217007+theBough@users.noreply.github.com>
2026-03-03 11:51:35 -0600

    Enhance Enemy class with movement capabilities
    Added movement functionality to the Enemy class by introducing xSpeed and ySpeed properties and a move method.

--- enemy.js
+++ enemy.js
@@ -1,11 +1,12 @@
-function Enemy(x,y,w,h,img){
+function Enemy(x,y,w,h,img, xSpeed, ySpeed){
   this.x = x;
   this.y = y;
   this.w = w;
   this.h = h;
   //this.col = col;
   this.img = loadImage(img);
-  
+  this.xSpeed = xSpeed;
+  this.ySpeed = ySpeed;
   
   this.display  = function(){
     push();
@@ -14,6 +15,10 @@ function Enemy(x,y,w,h,img){
     this.img.resize(this.w, this.h)
     image(this.img, this.x, this.y)
     pop()
+  }
+  this.move = function(){
+    this.x += xSpeed;
+    this.y += ySpeed;
   }
   
   
