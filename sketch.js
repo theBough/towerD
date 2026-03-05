e535d0914a6b7af818414c757550e0714025875c
corbin Boughen <50217007+theBough@users.noreply.github.com>
2026-03-05 11:14:00 -0600

    Implement left turns and update enemy spawn location
    Added left turn functionality and adjusted enemy position.

--- sketch.js
+++ sketch.js
@@ -1,17 +1,20 @@
 let p = [];
 let e;
 let map;
+//holder for leftTurns
+let lt;
 
 function setup() {
   createCanvas(640, 480);
   for (let i = 0; i < 5; i++) {
     p.push(new Player(200 + i * 20, 200, 20, 20, "green"));
   }
-  e = new Enemy(20, 100, 50,50,"images/cardinal1.png",1,0)
+  e = new Enemy(20, 170, 50,50,"images/cardinal1.png",1,0)
   map = loadImage("map.png")
+  placeLeftTurns();
 }
-
 function draw() {
+  frameRate(30)
   background(220);
   map.resize(640,400)
   image(map,0,0)
@@ -21,4 +24,14 @@ function draw() {
   }
   e.display()
   e.move();
+  leftTurns();
+}
+//------------------------------------------------------------
+//Below are self created functions
+function placeLeftTurns(){
+  lt = new LeftTurn(300,170,40,40);
+}
+function leftTurns(){
+  lt.display();
+  lt.collision(e);
 }
