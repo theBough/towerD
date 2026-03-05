58947769871020846c2e5bbe3fa8b6fdea60b81b
corbin Boughen <50217007+theBough@users.noreply.github.com>
2026-03-05 11:44:53 -0600

    Refactor leftTurns to use array for leftTurn instances

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ let p = [];
 let e;
 let map;
 //holder for leftTurns
-let lt,lt1,lt2,lt3;
+let lt = []
 
 function setup() {
   createCanvas(640, 480);
@@ -29,18 +29,16 @@ function draw() {
 //------------------------------------------------------------
 //Below are self created functions
 function placeLeftTurns(){
-  lt = new LeftTurn(300,170,40,40);
-  lt1 = new LeftTurn(300,85,40,40);
-  lt2 = new LeftTurn(150,85,40,40);
-  lt3 = new LeftTurn(150,340,40,40);
+  //this will be different for you!~!!!!!!!!!!!!!!!!!
+  lt.push(new LeftTurn(300,170,40,40));
+  lt.push(new LeftTurn(300,85,40,40));
+  lt.push(new LeftTurn(150,85,40,40));
+  lt.push(new LeftTurn(150,340,40,40));
 }
 function leftTurns(){
-  lt.display();
-  lt.collision(e);
-  lt2.display();
-  lt2.collision(e);
-  lt3.display();
-  lt3.collision(e);
-  lt1.display();
-  lt1.collision(e);
+  for(let i = 0; i < lt.length; i++){
+    lt[i].display();
+    lt[i].collision(e);
+    
+  }  
 }
