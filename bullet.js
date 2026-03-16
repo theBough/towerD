daf1be15bc07d3452cebb2a52fcd4ba51e715116
corbin Boughen <50217007+theBough@users.noreply.github.com>
2026-03-16 11:43:01 -0500

    Refactor Bullet class and related functions

--- bullet.js
+++ bullet.js
@@ -1,92 +1,30 @@
-//--------- Global Variables------------------
-let p = [];//array to hold the turrets
-let e = []; //array to hold the enemies
-let cardinalPics = [];//havent used it yet.
-let b = [] //array to hold the bullets
-let map;
-//holder for leftTurns
-let lt = [];
-let waveCount = 5;
-let stopWave;
-//--------------------------------------------
+function Bullet(x,y,w,h, xSpeed, ySpeed,id){
+  this.x = x;
+  this.y = y;
+  this.w = w;
+  this.h = h;
+  //this.col = col;
+  //this.img = loadImage(img);
+  this.xSpeed = xSpeed;
+  this.ySpeed = ySpeed;
+  this.id = id
 
-//-------functions that need to be here because of p5js ------------------
-function setup() {
-  createCanvas(640, 480);
-  for (let i = 0; i < 5; i++) {
-    p.push(new Player(20 + i * 22, 440, 20, 20, "pink"));
+  this.display  = function(){
+    push();
+    //fill(this.col)
+    rect(this.x, this.y, this.w, this.h);
+    //this.img.resize(this.w, this.h)
+    //image(this.img, this.x, this.y)
+    pop()
   }
-  sendWave();
-  map = loadImage("map.png");
-  placeLeftTurns();
-  
- 
-}
-function draw() {
-  background(220);
-  map.resize(640, 400);
-  image(map, 0, 0);
-  for (let i = 0; i < 5; i++) {
-    p[i].display();
-    p[i].update();
-  }
-  enemyStuff();
-  leftTurns();
-  bulletStuff();
-}
-function keyPressed(){
-  if(key === 'f'){
-    //user pressed the f key on the keyboard
-    b.push(new Bullet(p[0].x , p[0].y, 5,5,3,0,0))
-  }
-}
-
-//--------- Below are self created functions----------------------------
-function bulletStuff(){
-  for(let i =0 ; i < b.length ; i++){
-    b[i].display();
-    b[i].move();
-  }
-}//end bulletStuff
-function sendWave(){
-   stopWave = setInterval(createEnemy, 2000);
-}
-function enemyStuff() {
-  for (let i = 0; i < e.length; i++) {
-    e[i].display();
-    e[i].move();
-  } //end for loop
-} //end EnemyStuff
-function createEnemy() {
-  //create an enemy object , and push it onto the array
+  this.move = function(){
+    this.x += this.xSpeed;
+    this.y += this.ySpeed;
+    //check if we left the canvas, if yes delete it
+    if(this.x > width || this.x <0 || this.y > height || this.y <0){ 
     
-    e.push(new Enemy(20, 170, 50, 50, "images/cardinal1.png", 1, 0,e.length));
-   
-  if(e.length >= waveCount){
-    clearInterval(stopWave)
+    }
   }
   
   
-} //end createEnemy
-function loadAnimationPictures() {
-  let totalPics = 4;
-  for (let i = 0; i < totalPics; i++) {
-    let cardPic = loadImage(`images/`);
-  }
-} //end loadAnimations
-function placeLeftTurns() {
-  //this will be different for you!~!!!!!!!!!!!!!!!!!
-  lt.push(new LeftTurn(300, 170, 40, 40));
-  lt.push(new LeftTurn(300, 85, 40, 40));
-  lt.push(new LeftTurn(150, 85, 40, 40));
-  lt.push(new LeftTurn(150, 340, 40, 40));
-} //end placeLeftTurns
-function leftTurns() {
-  for (let i = 0; i < lt.length; i++) {
-    lt[i].display();
-    for(let j =0 ; j < e.length ; j++){
-      lt[i].collision(e[j]);
-    }//end loop
-    
-  }
-} //end  leftTurns
+}
