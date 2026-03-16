57d670c1f9472a328696e375f469c7b122bdc93f
corbin Boughen <50217007+theBough@users.noreply.github.com>
2026-03-16 11:36:11 -0500

    Refactor Bullet class display method and image handling

--- bullet.js
+++ bullet.js
@@ -1,21 +1,20 @@
-function Bullet(x,y,w,h,img, xSpeed, ySpeed,id){
+function Bullet(x,y,w,h, xSpeed, ySpeed,id){
   this.x = x;
   this.y = y;
   this.w = w;
   this.h = h;
   //this.col = col;
-  this.img = loadImage(img);
+  //this.img = loadImage(img);
   this.xSpeed = xSpeed;
   this.ySpeed = ySpeed;
   this.id = id
 
-  
   this.display  = function(){
     push();
     //fill(this.col)
-    //rect(this.x, this.y, this.w, this.h);
-    this.img.resize(this.w, this.h)
-    image(this.img, this.x, this.y)
+    rect(this.x, this.y, this.w, this.h);
+    //this.img.resize(this.w, this.h)
+    //image(this.img, this.x, this.y)
     pop()
   }
   this.move = function(){
