29d20b628188bb13c2338f204489ff640605e7da
corbin Boughen <50217007+theBough@users.noreply.github.com>
2026-03-23 11:54:32 -0500

    Update enemy.js

--- enemy.js
+++ enemy.js
@@ -1,4 +1,4 @@
-function Enemy(x,y,w,h,img, xSpeed, ySpeed,id, turning){
+function Enemy(x,y,w,h,img, xSpeed, ySpeed,id){
   this.x = x;
   this.y = y;
   this.w = w;
@@ -9,7 +9,14 @@ function Enemy(x,y,w,h,img, xSpeed, ySpeed,id, turning){
   this.ySpeed = ySpeed;
   this.id = id
 
-  
+  this.collisionWithEnemy = function(thisBullet){
+    if(thisBullet.x > this.x &&
+      thisBullet.x + thisBullet.w < this.x + this.w &&
+      thisBullet.y> this.y &&
+      thisBullet.y + thisBullet.h < this.y + this.h){
+      console.log("hit")
+    }
+  }
   this.display  = function(){
     push();
     //fill(this.col)
@@ -25,12 +32,8 @@ function Enemy(x,y,w,h,img, xSpeed, ySpeed,id, turning){
     //console.log(this.id)
     //check if we left the canvas, if yes delete it
     if(this.x > width || this.x <0 || this.y > height || this.y <0){ 
-     
       //let thisTemp = e.findIndex(this.id)
      e.splice(e.findIndex(a => a.id ===this.id),1)
-     
-
-    
     }
   }
   
